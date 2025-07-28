1048973623569b19c2f0fb41498628f783064eaf
enzozin <zin.enzo.mail@gmail.com>
2025-07-28 15:25:59 +0200

    Implement Test for FizzBuzz

--- exercises/4-fizzbuzz/test_fizzbuzz.py
+++ exercises/4-fizzbuzz/test_fizzbuzz.py
@@ -2,3 +2,23 @@ from src import fizzbuzz
 
 # def test_fizzbuzz():
 #     assert fizzbuzz(1)
+def test_1_should_return_1():
+    assert fizzbuzz(1) == '1'
+
+def test_2_should_return_2():
+    assert fizzbuzz(2) == '2'
+
+def test_3_should_return_Fizz():
+    assert fizzbuzz(3) == 'Fizz'
+
+def test_5_should_return_Buzz():
+    assert fizzbuzz(5) == 'Buzz'
+
+def test_15_should_return_FizzBuzz():
+    assert fizzbuzz(15) == 'FizzBuzz'
+
+def test_13_should_return_Fizz():
+    assert fizzbuzz(13) == 'Fizz'
+
+def test_25_should_return_Buzz():
+    assert fizzbuzz(25) == 'Buzz'
