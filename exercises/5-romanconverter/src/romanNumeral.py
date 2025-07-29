89f559b2761a6f66e9f3129c718e550190e65654
enzozin <zin.enzo.mail@gmail.com>
2025-07-29 14:10:07 +0200

    Ajout du calcul

--- exercises/5-romanconverter/src/romanNumeral.py
+++ exercises/5-romanconverter/src/romanNumeral.py
@@ -1,22 +1,28 @@
-# src/roman.py
+from unittest import result
+
 
 def romanNumeral(number: int) -> str:
+    result = ""
 
-#Tableau Valeurs
-
-    Entier_Romain : [
-        (1, 'I'),
-        (2, 'II'),
-        (3, 'III'),
-        (4, 'IV'),
-        (5, 'V'),
-        (6, 'VI'),
-        (7, 'VII'),
-        (8, 'VIII'),
-        (9, 'IX'),
-        (10, 'X'),
-        (50, 'L'),
-        (100, 'C'),
-        (500, 'D'),
+    Entier_Romain = [
         (1000, 'M'),
-]
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I')
+    ]
+    for valeur, symbole in Entier_Romain:
+        while number >= valeur:
+            result += symbole
+            number -= valeur
+
+    return result
+
