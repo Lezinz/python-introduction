e12cbc6dad48ce4e8e49b59fdcd6122787cb8134
enzozin <zin.enzo.mail@gmail.com>
2025-07-29 14:24:27 +0200

    Tableau Valeurs (symbole, valeur)

--- exercises/5-romanconverter/src/romanNumeral.py
+++ exercises/5-romanconverter/src/romanNumeral.py
@@ -35,3 +35,19 @@ def romanNumeral(number: int) -> str:
 def romantoint(symbole: str) -> int:
     result = ""
 
+    Romain_Entier = [
+        ('M', 1000),
+        ('CM', 900),
+        ('D', 500),
+        ('CD', 400),
+        ('C', 100),
+        ('XC', 90),
+        ('L', 50),
+        ('XL', 40),
+        ('X', 10),
+        ('IX', 9),
+        ('V', 5),
+        ('IV', 4),
+        ('I', 1)
+    ]
+
