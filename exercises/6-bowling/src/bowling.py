0d4e560f025db7658dc2ec58d12fe6c7886a932c
enzozin <zin.enzo.mail@gmail.com>
2025-07-30 14:05:03 +0200

    Transformation chaine de caractère

--- exercises/6-bowling/src/bowling.py
+++ exercises/6-bowling/src/bowling.py
@@ -1,3 +1,22 @@
 #write code here
-def jn():
-    return 1
+from .regular_frame import RegularFrame
+
+class Bowling():
+    frames: list[RegularFrame]
+
+    def __init__(self, frame: str):
+        frame_list = frame.split()
+
+        tempFrame = []
+        for frame_text in frame_list:
+            tempFrame.append(RegularFrame(frame_text))
+
+        self.frames = tempFrame
+
+    # transformer la chaine de character
+
+    def calculate(self) -> int:
+        total = 0
+        for frame in self.frames:
+            total += frame.calculate()
+        return total
