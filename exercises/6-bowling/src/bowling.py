f9b2174dfd9db18c5e6d1f9797b8ad6362a2a429
enzozin <zin.enzo.mail@gmail.com>
2025-07-31 09:03:51 +0200

    ajout des calcules pour le spare

--- exercises/6-bowling/src/bowling.py
+++ exercises/6-bowling/src/bowling.py
@@ -1,22 +1,25 @@
-#write code here
 from .regular_frame import RegularFrame
+from .spare_frame import SpareFrame
 
-class Bowling():
-    frames: list[RegularFrame]
-
-    def __init__(self, frame: str):
-        frame_list = frame.split()
-
-        tempFrame = []
-        for frame_text in frame_list:
-            tempFrame.append(RegularFrame(frame_text))
-
-        self.frames = tempFrame
-
-    # transformer la chaine de character
+class Bowling:
+    def __init__(self, frame_str: str):
+        self.frames = frame_str.strip().split()
 
     def calculate(self) -> int:
         total = 0
-        for frame in self.frames:
-            total += frame.calculate()
+        i = 0
+        while i < 10:
+            frame = self.frames[i]
+            if '/' in frame:
+                previous_throw = int(frame[0]) if frame[0] != '-' else 0
+                if i + 1 < len(self.frames):
+                    next_frame = self.frames[i + 1]
+                else:
+                    next_frame = frame[2:] if len(frame) > 2 else "0"
+                spare = SpareFrame(previous_throw, next_frame)
+                total += spare.calculate()
+            else:
+                regular = RegularFrame(frame)
+                total += regular.calculate()
+            i += 1
         return total
