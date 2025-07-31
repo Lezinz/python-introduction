400762ec924587be4b0e07973cf30fb3a3bb2e46
enzozin <zin.enzo.mail@gmail.com>
2025-07-31 16:14:10 +0200

    Ajout des tests de direction

--- exercises/7-marsrover/test_marsrover.py
+++ exercises/7-marsrover/test_marsrover.py
@@ -1,50 +1,26 @@
-from src.rover import Rover
+from src import MarsRover, Direction
 
 def test_init_rover():
-    r = Rover(2, 4, "N")
-    assert (r.x, r.y) == (2, 4)
-    assert r.direction == "N"
+    rover = MarsRover((2, 4), Direction.NORTH)
+    assert rover.coordinates == (2, 4)
+    assert rover.direction == Direction.NORTH
 
-def test_advance_rover():
-    class Carte:
-        grille = [["🟩"]*5 for _ in range(5)]
-    r = Rover(2, 4, "N")
-    r.avancer(Carte())
-    assert (r.x, r.y) == (2, 3)
+def test_advance_rover_north():
+    rover = MarsRover((2, 4), Direction.NORTH)
+    rover.advance()
+    assert rover.coordinates == (2, 3)
 
-def test_turn_left():
-    r = Rover(2, 4, "N")
-    r.tourner_gauche()
-    assert r.direction == "W"
-    r.tourner_gauche()
-    assert r.direction == "S"
+def test_advance_rover_south():
+    rover = MarsRover((2, 4), Direction.SOUTH)
+    rover.advance()
+    assert rover.coordinates == (2, 5)
 
-def test_turn_right():
-    r = Rover(2, 4, "N")
-    r.tourner_droite()
-    assert r.direction == "E"
-    r.tourner_droite()
-    assert r.direction == "S"
+def test_advance_rover_east():
+    rover = MarsRover((2, 4), Direction.EAST)
+    rover.advance()
+    assert rover.coordinates == (3, 4)
 
-def test_advance_blocked_by_obstacle():
-    grille = [
-        ["🟩","🟩","🌳","🟩","🟩"],
-        ["🟩","🟩","🟩","🟩","🟩"],
-        ["🟩","🟩","🟩","🌳","🟩"],
-        ["🟩","🌳","🟩","🟩","🟩"],
-        ["🟩","🟩","🟩","🟩","🟩"],
-    ]
-
-    class Carte:
-        def __init__(self):
-            self.grille = grille
-
-    carte = Carte()
-
-    r = Rover(2, 3, "E")
-    r.avancer(carte)
-    assert (r.x, r.y) == (3, 3)
-
-    r = Rover(2, 2, "E")
-    r.avancer(carte)
-    assert (r.x, r.y) == (2, 2)
+def test_advance_rover_west():
+    rover = MarsRover((2, 4), Direction.WEST)
+    rover.advance()
+    assert rover.coordinates == (1, 4)
