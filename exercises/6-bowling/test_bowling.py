d4cf73b1eba3e92ddaf1216a4234871623b5cd61
enzozin <zin.enzo.mail@gmail.com>
2025-07-31 12:04:40 +0200

    Ajout de test pour le bowling

--- exercises/6-bowling/test_bowling.py
+++ exercises/6-bowling/test_bowling.py
@@ -28,5 +28,22 @@ def test_tous_les_strikes():
     bowling = Bowling("X X X X X X X X X X X X")
     assert bowling.calculate() == 300
 
+def test_strike_suivi_de_9_et_0():
+    bowling = Bowling("X 90 -- -- -- -- -- -- -- --")
+    assert bowling.calculate() == 28  # 10 + 9 + 0 = 19 + 9
 
+def test_spare_suivi_de_9_et_0():
+    bowling = Bowling("5/ 90 -- -- -- -- -- -- -- --")
+    assert bowling.calculate() == 10 + 9 + 9
 
+def test_spare_en_dernier_avec_bonus():
+    bowling = Bowling("-- -- -- -- -- -- -- -- -- 5/5")
+    assert bowling.calculate() == 10 + 5
+
+def test_strike_en_dernier_avec_bonus():
+    bowling = Bowling("-- -- -- -- -- -- -- -- -- X 11")
+    assert bowling.calculate() == 10 + 1 + 1
+
+def test_mixte_strike_spare_normal():
+    bowling = Bowling("X 5/ 72 9- X 3/ 61 X X 44")
+    assert bowling.calculate() == 148
